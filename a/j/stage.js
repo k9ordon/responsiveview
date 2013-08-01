70464a9d4ce272119c88eaf44654bf2e7232c032
Klemens Gordon <github@k94n.com>
2013-08-01 18:43:06 +0200

    wörterseesession stage: nowrap, implemented updateDeviceSet & updateDeviceScale methods

--- a/j/stage.js
+++ a/j/stage.js
@@ -13,8 +13,66 @@ p.init = function() {
 
 p.events = function() {}
 
-p.loadHref = function(href) {
+p.updateHref = function(href) {
 	console.log(['stage loaded', href, this, _stage.$el]);
-	
-	new device().init();
+
+	if(this.devices.length == 0) {
+		this.updateDeviceSet(_mainbar.getDeviceSet());
+	}
+
+	for(var i = 0; i < this.devices.length; i++) {
+		console.log(['stage update device href', i, href]);
+		this.devices[i].updateHref(href);
+	}
+}
+
+p.updateDeviceSet = function(deviceSet) {
+	console.log(['stage change device set', this.devices.length, deviceSet.devices.length]);
+
+	// 1 = 4 - 3 // -2 = 1 - 3
+	var devices = deviceSet.devices,
+		currentCount = this.devices.length,
+		newCount = devices.length,
+		iterations = newCount >= currentCount ? newCount : currentCount,
+		newDevices = [];
+
+	for(var i = 1; i <= iterations; i++) {
+		console.log(['stage update device', iterations, i, newCount, currentCount, devices[i]]);
+
+		if(i <= newCount && i > currentCount) {
+			// create device
+			console.log('new');
+			newDevices.push(new device().init(devices[i-1]));
+		}
+		//		4	4				4	3
+		else if(i <= currentCount && i > newCount) {
+			// remove device
+			console.log('remove');
+			this.devices[i-1].destroy();
+		}
+		else {
+			// update device
+			console.log('update');
+			newDevices.push(this.devices[i-1]);
+			this.devices[i-1].update(devices[i-1]);
+		}
+	}
+
+	this.devices = newDevices;
+	this.updateDeviceScale(_mainbar.getDeviceScale());
+	console.log(['device deploy done.', newDevices]);
+}
+
+p.updateDeviceScale = function(scale) {
+	for(var i = 0; i < this.devices.length; i++) {
+		this.devices[i].updateScale(scale);
+	}
+}
+
+p.addDevices = function() {
+
+}
+
+p.removeDevices = function() {
+
 }
