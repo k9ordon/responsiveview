f82ade7fa707f63a21a1b2c564b62cff49c1e5eb
Klemens Gordon <github@k94n.com>
2013-08-07 10:22:42 +0200

    updated device sets

--- a/d/deviceSets.js
+++ a/d/deviceSets.js
@@ -1,19 +1,31 @@
 var deviceSets = [
     {
-        name : 'Default Devices',
+        name : 'Simple Devices',
         devices : [
-            { name: 'iphone 4', w: 320, h:416, type:'phone' },
-            { name: 'Nexus 4', w: 382, h:592, type:'phone' },
-            { name: 'Samsung Galaxy SII', w: 480, h:480, type:'phone' },
+            { name: 'Phone', w: 320, h:480, type:'phone', rotated: { w:416, h:320} },
+            { name: 'Small Tablet', w: 600, h: 1000, type:'tablet' },
+            { name: 'Tablet', w: 760, h: 1024, type:'tablet' },
+            { name: '17"', w: 1024, h:768, type:'laptop' },
+        ]
+    },
+    {
+        name : 'Common Devices',
+        devices : [
+            { name: 'iPhone 4', w: 320, h:480, type:'phone', rotated: { w:416, h:320} },
+            { name: 'iPhone 5', w: 320, h:568, type:'phone', rotated: { w:568, h:320} },
+            { name: 'BlackBerry Curve', w: 480, h:360, type:'phone' },
+            { name: 'Nexus 4', w: 382, h:592, type:'phone', rotated: { w:416, h:320} },
+            { name: 'Apple iPad', w: 768, h:1024, type:'tablet' },
+            { name: '17"', w: 1024, h:768, type:'laptop' },
         ]
     },
     {
         name : 'Breakpoints',
         devices : [
-            { name: 'Phone', w: 320, h: 480, type:'phone' },
-            { name: 'Small Tablet', w: 600, h: 1000, type:'phone' },
-            { name: 'Tablet', w: 760, h: 1024 },
-            { name: 'Desktop', w: 1024, h: 768 },   
+            { name: '1', w: 320, h: 480, type:'phone' },
+            { name: '2', w: 600, h: 1000, type:'tablet' },
+            { name: '3', w: 760, h: 1024 },
+            { name: '4', w: 1024, h: 768 },   
         ]
     }
 ];
