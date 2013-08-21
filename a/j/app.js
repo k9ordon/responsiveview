f309c052a9f8821324f1668f564c789e1a5bb334
Klemens Gordon <github@k94n.com>
2013-08-21 22:48:30 +0200

    new startpage

--- a/j/app.js
+++ a/j/app.js
@@ -6,5 +6,5 @@ _mainbar.init();
 
 var _stage = new stage();
 _stage.init();
-//_stage.updateHref('hello.html');
-_stage.updateHref('http://k94n.duro');
+_stage.updateHref('http://duro/responsiveview/hello.html');
+//_stage.updateHref('http://k94n.duro');
