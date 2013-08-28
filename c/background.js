3a4f89c17fbe4ea867fb55868c7982e050a1f62f
Klemens Gordon <github@k94n.com>
2013-08-28 10:08:29 +0200

    added permission for webRequest(blocking)

--- c/background.js
+++ c/background.js
@@ -12,3 +12,32 @@ var onRequest = function (request, sender, sendResponse) {
     sendResponse({});
 };
 chrome.extension.onRequest.addListener(onRequest);
+
+
+/*
+var requestFilter = {
+        urls: [ "<all_urls>" ]
+    },
+    
+    extraInfoSpec = ['requestHeaders','blocking'],
+    
+    handler = function( details ) {
+
+        var headers = details.requestHeaders,
+        blockingResponse = {};
+
+        if(details.type !== 'sub_frame') return blockingResponse;
+        console.log('webrequest', details);
+
+        for( var i = 0, l = headers.length; i < l; ++i ) {
+            if( headers[i].name == 'User-Agent' ) {
+                headers[i].value = 'New User-Agent';
+                break;
+            }
+        }
+
+        blockingResponse.requestHeaders = headers;
+        return blockingResponse;
+    };
+chrome.webRequest.onBeforeSendHeaders.addListener( handler, requestFilter, extraInfoSpec );
+*/
